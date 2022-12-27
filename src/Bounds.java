93ee50b761edcc8c718ac849540885dfa2aa7965
Liam Hess <liamskydamien@gmail.com>
2022-12-27 23:03:31 +0100

    Add Bounds

--- src/Bounds.java
+++ src/Bounds.java
@@ -4,7 +4,9 @@
  * Matrikelnummer:
  */
 
-public class Bounds {
-	public void copy(Paar src, Paar dst){
+public class Bounds{
+	public static <T, E extends T> void copy(Paar<E> src, Paar<T> dst){
+		dst.p1 = src.p1;
+		dst.p2 = src.p2;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Paar < Integer > src = new Paar < >(1 ,2) ;
+        Paar < Number > dst = new Paar < >(3 ,4) ;
+        System.out . println ( " src = "+ src +" ndst ␣ vor ␣ copy = "+ dst );
+        Bounds.copy ( src , dst );
+        System.out.println ( " dst ␣ nach ␣ copy = "+ dst );
+
     }
 }
