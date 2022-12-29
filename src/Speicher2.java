98a322324bae9329357fb3ddf4621a1f28da6620
Liam Hess <liamskydamien@gmail.com>
2022-12-29 13:16:05 +0100

    Add List

--- src/Speicher2.java
+++ src/Speicher2.java
@@ -3,6 +3,71 @@
  * Name:
  * Matrikelnummer:
  */
-public class Speicher2 {
+public class Speicher2<T> implements SpeicherIF<T>{
+    private static class Node<T>{
+        private final T value;
+        private Node<T> next = null;
+        public Node(T e){
+            value = e;
+        }
 
+        public void setNext(Node<T> n){
+            next = n;
+        }
+
+        public T getValue(){
+            return value;
+        }
+
+        public Node<T> getNextNode(){
+            return next;
+        }
+
+    }
+
+    Node<T> first = null;
+    int size = 0;
+    int counter = 1;
+
+    @Override
+    public void insert(T e) {
+        if(counter < 3){
+            if(first == null){
+                first = new Node<>(e);
+            }
+            else {
+                Node<T> newNode = new Node<>(e);
+                newNode.setNext(first);
+                first = newNode;
+            }
+            size++;
+            counter++;
+        }
+        else {
+            counter = 1;
+        }
+    }
+
+    @Override
+    public T remove() {
+        if(first != null) {
+            T retValue = first.getValue();
+            first = first.getNextNode();
+            size--;
+            return retValue;
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
 }
