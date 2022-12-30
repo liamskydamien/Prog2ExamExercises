f8b20cb63b5aa587cc451c9cf0ba4df9a3363a23
Liam Hess <liamskydamien@gmail.com>
2022-12-30 18:13:55 +0100

    Adding Speicher3

--- src/Speicher3.java
+++ src/Speicher3.java
@@ -3,6 +3,57 @@
  * Name:
  * Matrikelnummer:
  */
-public class Speicher3 {
+public class Speicher3<T> implements SpeicherIF<T>{
 
+    @SuppressWarnings("unchecked")
+    T[] speicher = (T[]) new Object[10];
+    int position = 0;
+    int counter = 0;
+
+    @Override
+    public void insert(T e) {
+        if(position < 9) {
+            speicher[++position] = e;
+        }
+        else {
+            speicher[0] = e;
+            position = 0;
+        }
+        counter++;
+    }
+
+    @Override
+    public T remove() {
+        T retValue = speicher[position];
+        speicher[position] = null;
+        if(position == 0){
+            position = 9;
+        }
+        else {
+            position--;
+        }
+        counter--;
+        return retValue;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return speicher[position] == null;
+    }
+
+    @Override
+    public int size() {
+        return counter;
+    }
+
+    @Override
+    public String toString(){
+        String returnValue = "[";
+        for (T element : speicher){
+            if(element != null) {
+                returnValue += element + ",";
+            }
+        }
+        return isEmpty()? "[]" : (returnValue.substring(0,returnValue.length() -1) + "]");
+    }
 }
---
+++ src/Speicher3UnitTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Speicher3UnitTest {
+    Speicher3<Character> speicher2;
+    final char[] liste = {'a', 'b', 'c', 'd', 'e', 'a', 'b'};
+
+    @BeforeEach
+    void setup() {
+        speicher2 = new Speicher3<>();
+    }
+
+    @AfterEach
+    void teardown() {
+        speicher2 = null;
+    }
+
+    @Test
+    void insert() {
+        for (Character character : liste) {
+            speicher2.insert(character);
+            System.out.println(speicher2);
+        }
+    }
+
+    @Test
+    void remove() {
+        for (Character character : liste) {
+            speicher2.insert(character);
+            System.out.print(speicher2);
+        }
+        while (!speicher2.isEmpty()) {
+            speicher2.remove();
+            System.out.print(speicher2);
+        }
+    }
+}
