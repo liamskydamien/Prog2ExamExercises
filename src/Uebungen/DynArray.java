6cc663c536e96f013cb862ef30363088aa0cd3bb
Liam Hess <liamskydamien@gmail.com>
2023-01-28 17:49:26 +0100

    Adding Stack DynArray

--- src/Uebungen/DynArray.java
+++ src/Uebungen/DynArray.java
@@ -2,6 +2,7 @@ package Uebungen;
 
 public class DynArray<T> implements Speicher<T> {
     private T[] speicher;
+    private int size = 0;
 
     @SuppressWarnings("unchecked")
     public DynArray(int size){
@@ -10,21 +11,49 @@ public class DynArray<T> implements Speicher<T> {
 
     @Override
     public void push(T e) {
+        if(isEmpty()){
+            speicher[0] = e;
+        }
+        else{
+            if(size == speicher.length){
+                createDoubleArray();
+            }
+            speicher[size++] = e;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void createDoubleArray(){
+        T[] newArray = (T[]) new Object[size * 2];
+        if (size >= 0) System.arraycopy(speicher, 0, newArray, 0, size);
+        speicher = newArray;
+    }
+
+    private void createHalfArray(){
 
     }
 
     @Override
     public T pop() {
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        else {
+            T retValue = speicher[--size];
+            if(size <= speicher.length / 4){
+                createHalfArray();
+            }
+            return retValue;
+        }
     }
 
     @Override
     public T get(int i) {
-        return null;
+        return speicher[i];
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return speicher[0] == null;
     }
 }
