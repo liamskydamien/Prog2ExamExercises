a4b350c54c1cc2e94de812602fc11d717a0c0b39
Liam Hess <liamskydamien@gmail.com>
2023-01-28 18:23:29 +0100

    Test 2d sKIP

--- src/IteratorExcercise/IteratorTest.java
+++ src/IteratorExcercise/IteratorTest.java
@@ -6,10 +6,10 @@ import java.util.function.IntBinaryOperator;
 public class IteratorTest {
     public static void main(String[] args) {
       Integer[][] integers = {{1,2,3},{6,5,4},{7,8,9}};
-//        Iterator<Integer> iterator = new SnakeIterator<>(integers);
-//        while (iterator.hasNext()){
-//            System.out.println(iterator.next());
-//        }
+        Iterator<Integer> iterator = new DoubleSkip(integers);
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
 //
 //        Integer[] integers1 = {1,2,3,4,5,6,7,8,9,10};
 //        Iterator<Integer> iterator1 = new SkipIterator<>(integers1);
@@ -17,9 +17,9 @@ public class IteratorTest {
 //            System.out.println(iterator1.next());
 //        }
 
-        Iterator<Integer> iterator2= new SpaltenIterator<>(integers);
-        while (iterator2.hasNext()){
-            System.out.println(iterator2.next());
-        }
+//        Iterator<Integer> iterator2= new SpaltenIterator<>(integers);
+//        while (iterator2.hasNext()){
+//            System.out.println(iterator2.next());
+//        }
     }
 }
